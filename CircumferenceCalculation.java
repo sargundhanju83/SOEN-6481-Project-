43881538bfe18823c5c0fd928cba9f058a94b67f
sargundhanju83 <52549930+sargundhanju83@users.noreply.github.com>
2019-07-30 17:24:40 -0400

    Update CircumferenceCalculation.java

--- CircumferenceCalculation.java
+++ CircumferenceCalculation.java
@@ -4,14 +4,23 @@ public class CircumferenceCalculation
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Press CRF to calculate circumference");
         String s=sc.nextLine();
         s=s.toUpperCase();
         if(s.equals("CRF"))
         {
+            System.out.println("Enter the radius");
             String radius=sc.nextLine();
             double r=Double.parseDouble(radius);
+            if(r>0)
+            {
             double result=2*3.14*r;
             System.out.println(result);
+            }
+            else
+            {
+                System.out.println("Invalid input");
+            }
         }
     }
 }
